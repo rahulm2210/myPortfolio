61a5f9f12527b34ae95b2319d9dd61822cea2e7a
rahulm2210 <rahul486mejari@gmail.com>
2024-09-05 19:18:19 +0530

    contact updated again

--- myPortfolio/src/assets/components/Contact/Contact.jsx
+++ myPortfolio/src/assets/components/Contact/Contact.jsx
@@ -26,7 +26,7 @@ const Contact = () => {
     }).then((res) => res.json());
 
     if (res.success) {
-      console.log("Success", res);
+      //console.log("Success", res);
       Swal.fire({
         title: "Success!",
         text: "Message Sent Successfully!",
